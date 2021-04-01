95d452c08810eace75647496da14e8b62a6f232b
BigBoi077 <58580199+BigBoi077@users.noreply.github.com>
2021-03-31 22:47:24 -0400

    Initial commit

---
+++ app/src/main/java/cegepst/example/lunatics/MainActivity.kt
@@ -0,0 +1,11 @@
+package cegepst.example.lunatics
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
