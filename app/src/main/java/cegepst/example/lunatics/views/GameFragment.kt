3775d372ac4fac3e11b625995ef1813ded29bfce
BigBoi077 <58580199+BigBoi077@users.noreply.github.com>
2021-04-08 22:23:28 -0400

    Made fragment for activities and SingleGameActivity.kt for the button click event

---
+++ app/src/main/java/cegepst/example/lunatics/views/GameFragment.kt
@@ -0,0 +1,52 @@
+package cegepst.example.lunatics.views
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
+import cegepst.example.lunatics.R
+import cegepst.example.lunatics.viewModels.MainViewModel
+
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+class GameFragment : Fragment() {
+    private lateinit var viewModel: MainViewModel
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+        viewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_game, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<RecyclerView>(R.id.listGames)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            GameFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
---
+++ app/src/main/java/cegepst/example/lunatics/views/SingleGameActivity.kt
@@ -0,0 +1,12 @@
+package cegepst.example.lunatics.views
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import cegepst.example.lunatics.R
+
+class SingleGameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_single_game)
+    }
+}
